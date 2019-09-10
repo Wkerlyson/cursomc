3492fa6a15cb81987d03f8dbf402f60d498f504a
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2019-09-10 18:29:15 -0300

    Product and association ManyToMany

--- src/main/java/com/wkprojects/CursomcApplication.java
+++ src/main/java/com/wkprojects/CursomcApplication.java
@@ -2,19 +2,25 @@ package com.wkprojects;
 
 import java.util.Arrays;
 
+import org.apache.tomcat.jni.Proc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wkprojects.domain.Category;
+import com.wkprojects.domain.Product;
 import com.wkprojects.repositories.CategoryRepository;
+import com.wkprojects.repositories.ProductRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
 		
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
