ad16d0b16b68253763ed17fe62cca11e8ce13715
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2019-10-01 20:52:47 -0300

    PUT, DELETE and GET for Client

--- src/main/java/com/wkprojects/resources/ClientResource.java
+++ src/main/java/com/wkprojects/resources/ClientResource.java
@@ -1,13 +1,24 @@
 package com.wkprojects.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wkprojects.domain.Client;
+import com.wkprojects.dto.ClientDTO;
 import com.wkprojects.services.ClientService;
 
 @RestController()
@@ -17,10 +28,44 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	@GetMapping
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<Client> categories = service.findAll();
+		List<ClientDTO> listDto = categories.stream().map(obj -> new ClientDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Client> find(@PathVariable Integer id ){
 		Client client = service.find(id);
 		return ResponseEntity.ok(client);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClientDTO clienteDto, @PathVariable Integer id) {
+		Client cliente = service.fromDto(clienteDto);
+		service.update(cliente, id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClientDTO>> findPage(
+		@RequestParam(value = "page", defaultValue = "0") Integer page, 
+		@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+		@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Client> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClientDTO> listDto = list.map(obj -> new ClientDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+
 	}
 	
 }
--- src/main/java/com/wkprojects/services/ClientService.java
+++ src/main/java/com/wkprojects/services/ClientService.java
@@ -1,12 +1,19 @@
 package com.wkprojects.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wkprojects.domain.Client;
+import com.wkprojects.dto.ClientDTO;
 import com.wkprojects.repositories.ClientRepository;
+import com.wkprojects.services.exceptions.DataIntegretyException;
 import com.wkprojects.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +26,38 @@ public class ClientService {
 		Optional<Client> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Object not found! Id: " + id + ", Type: " + Client.class.getName()));
+	}
+	
+	public Client update(Client cliente, Integer id) {
+		Client newObj = find(id);
+		updateData(newObj, cliente);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Unable to delete a cliente that has orders and address");
+		}
+	}
+
+	public List<Client> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Client fromDto(ClientDTO dto) {
+		return new Client(dto.getId(), dto.getName(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Client newObj, Client obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
